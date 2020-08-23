032d1a3d06539e1fee68f17cb3da31ddc3065bad
Freddie Lumor <freddielumor@gmail.com>
2020-08-23 14:22:42 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => <div className="app">APP</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react", "@babel/preset-env"],
+          plugins: [],
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      inject: "body",
+    }),
+  ],
+};
